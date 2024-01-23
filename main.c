591e76fc58c11fe02ed46ddeef6bb6768be9ef52
uriel <uriel1999@gmail.com>
2024-01-23 19:06:19 +0200

    changed the main to print as the tests require

--- main.c
+++ main.c
@@ -7,36 +7,36 @@ int main()
     unsigned int num2;
     printf("Enter 2 numbers: \n");
     scanf("%u %u", &num1, &num2);
-    printf("\nThe Armstrong numbers are: ");
+    printf("\nThe Armstrong numbers are:");
     for (int i = num1; i <= num2; i++)
     {
         if(isArmstorng(i))
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\nThe Palindrome numbers are: ");
+    printf("\nThe Palindromes are:");
     for (int i = num1; i <= num2; i++)
     {
         if(isPalindrome(i))
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("The Prime numbers are: ");
+    printf("\nThe Prime numbers are:");
     for (int i = num1; i <= num2; i++)
     {
         if(isPrime(i))
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\nThe strong numbers are: ");
+    printf("\nThe Strong numbers are:");
     for (int i = num1; i <= num2; i++)
     {
         if(isStrong(i))
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
